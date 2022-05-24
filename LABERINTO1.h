6866e42ce22621e42dd0010bcb6cf5c0a6680a27
MIQUELZAVALA <99816730+MIQUELZAVALA@users.noreply.github.com>
2022-05-24 12:22:13 +0200

    Update and rename LABERINTO1.h to lab.h

--- LABERINTO1.h
+++
@@ -1,7 +0,0 @@
-#ifndef _LABERINTO1
-#define _LABERINTO1
-
-void dificultadlab (int a, int b);
-
-#include  "LABERINTO1.c"
-#endif
---
+++ lab.h
@@ -0,0 +1,12 @@
+#ifndef _LAB_H_
+#define _LAB_H_
+
+# include <stdio.h>
+# include "lab.c"
+
+void ModoFacil(int a, int b);
+void ModoMedio(int a, int b);
+void ModoDificil(int a, int b);
+void ModoExtremo(int a, int b);
+
+#endif
