78d4843ed053ea1b7e5e07c9d390a86a9aec91b8
yeraymunozupm <99816468+yeraymunozupm@users.noreply.github.com>
2022-05-25 19:11:00 +0200

    Create labequipo.h

---
+++ labequipo.h
@@ -0,0 +1,15 @@
+#ifndef _LAB_H_
+#define _LAB_H_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+# include "labequipo.c"
+
+void ModoFacil(int a, int b);
+void ModoMedio(int a, int b);
+void ModoDificil(int a, int b);
+void ModoExtremo(int a, int b);
+void RecorrerLab(char a[20][20]);
+#endif
