d32c396d51d016ac05377158bd670c7c3d3c6556
sergiosimonherrera <sergio.simon@alumnos.upm.es>
2022-05-25 17:15:09 +0200

    upodate

---
+++ Cronometro.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+#include "lab.h"
+
+void cronometro(int a)
+{
+    int segundos = 0;
+    int minutos = 0;
+    int horas = 0;
+
+
+    printf ("iniciar: ");
+    system("pause ->NUL");
+
+    while (horas!=a)
+    {
+        segundos++;
+
+        if (segundos==60)
+        {
+            segundos=0;
+            minutos++;
+            if (minutos=60)
+            {
+                minutos=0;
+                horas++;
+            }
+
+        }
+        system("cls");
+        printf("\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t\t\t[ %.2d:%.2d:%.2d ]", minutos, horas, segundos);
+        Sleep(1000);
+    }
+
+
+
+
+}
---
+++ Cronometro.h
@@ -0,0 +1,9 @@
+#ifndef _CRONOMETRO_H_
+#define _CRONOMETRO_H_
+
+# include <stdio.h>
+# include "Cronometro.c"
+
+void cronometro(int a);
+
+#endif
