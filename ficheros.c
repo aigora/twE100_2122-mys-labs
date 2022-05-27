bdc2fc76624b74be335a73e6d25550edc52be25b
yeraymunozupm <99816468+yeraymunozupm@users.noreply.github.com>
2022-05-27 20:37:14 +0200

    Update ficheros.c

--- ficheros.c
+++ ficheros.c
@@ -21,3 +21,25 @@ void historia(int a)
         }
     printf("\n\n");
 }
+
+void finalhistoria(int a)
+{
+    int i=0;
+    char letra[10000];
+    printf("\n\n");
+    FILE *pf;
+    pf = fopen("desenlacehistoria.txt", "r");
+
+    if(pf == NULL)
+        {
+            printf("Error al encontrar el archivo");
+            return (-1);
+        }
+
+    while(fscanf(pf, "%c", &letra[i]) != EOF)
+        {
+            printf("%c", letra[i]);
+            i++;
+        }
+    printf("\n\n");
+}
