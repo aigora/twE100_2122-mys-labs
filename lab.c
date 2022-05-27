bfcdf6a8a2f7c38b16ad6401102cc43d94bca010
sergiosimonherrera <sergio.simon@alumnos.upm.es>
2022-05-27 10:39:43 +0200

    Update lab.c

--- lab.c
+++ lab.c
@@ -1,4 +1,4 @@
-#include "lab.h"
+#include <stdio.h>
 
 // a será del tamaño, segun la dificultad del laberinto
 void ModoFacil(int a, int b)
@@ -54,12 +54,7 @@ void ModoMedio(int a, int b)
       {"##################################################"}
       };
 
-   for (i=0; i<20; i++){
-        for (j=0; j<50; j++)
-            {
-                printf("%c", laberinto1[i][j]);
-            }
-         printf("\n");}
+   RecorrerLab(laberinto1);
 
 }
 
@@ -194,4 +189,108 @@ void ModoExtremo(int a, int b)
             }
          printf("\n");}
 
+}
+
+
+void RecorrerLab(char a[20][20])
+{
+
+    int x, y, i, j;
+
+
+    char tecla, linea, asterisco;
+
+    linea=219;
+    asterisco='#';
+    for(i=0;i<50;i++) //PARA INTERCAMBIAR ASTERISCOS
+    {
+        for(j=0;j<50;j++)
+        {
+            if(a[i][j]==asterisco)
+            {
+              a[i][j]=linea;
+            }
+           //intercambia(&asterisco,&linea);
+        }
+
+    }
+
+//aqui hay que imprimir la direccion de memoria
+
+    do
+    {
+        printf("mete un punto inicial del plano del laberinto (mayor que 0, menor que los limites del laberinto):\n");
+        scanf("%i %i", &x, &y);
+    }while(x <= 0 || y <= 0);
+
+    system("cls");//borra la pantalla para que no se acumulen los puntos introducidos
+
+    //imprimimos el laberinto con las condiciones iniciales
+        //el 5 es nº de casillas q vemos en vertical
+        //el 5 es el nº de casillas q vemos en horizontal
+        for(i=x;i<x+4;i++)
+        {
+            for(j=y;j<y+6;j++)
+            {
+                printf("%c", a[i-1][j-3]);
+            }
+            printf("\n");
+        }
+
+    //Esto va desplazando la pelota por el laberinto, mientras su posicion sea distinta de los extremos del lab (28 y 18) casillas
+    while(x!=28 || y!= 18)
+    {
+        tecla=getch();//lo mismo que el scanf para caracteres
+
+            //secuencia de movimientos de las teclas
+            if(tecla == 's')
+                {
+                    if(a[x+1][y] != linea)
+                    {
+                        a[x][y] = ' ';
+                        x++;
+                        a[x][y]='o';
+                    }
+                }
+            if(tecla == 'w')
+                {
+                    if(a[x-1][y] != linea)
+                    {
+                        a[x][y] = ' ';
+                        x--;
+                        a[x][y]='o';
+                    }
+                }
+            if(tecla == 'd')
+                {
+                    if(a[x][y+1] != linea)
+                    {
+                        a[x][y] = ' ';
+                        y++;
+                        a[x][y]='o';
+                    }
+                }
+            if(tecla == 'a')
+                {
+                    if(a[x][y-1] != linea)
+                    {
+                        a[x][y] = ' ';
+                        y--;
+                        a[x][y]='o';
+                    }
+                }
+            system("cls");//para que se borre el laberinto anterior y la tecla metida para mover la bola
+
+                    //imprimimos el nuevo laberinto (del mismo modo) con la posicion de la tecla añadida
+                    for(i=x;i<x+4;i++)
+                    {
+                        for(j=y;j<y+6;j++)
+                        {
+                            printf("%c", a[i-1][j-3]);
+                        }
+                        printf("\n");
+                    }
+    }//fin del while
+
+    system("cls");//limpia la pantalla para hacer todo otra vez y que no se acumulen los laberitos
 }
