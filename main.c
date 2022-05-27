e7b33c034838a330c077fbcfb719264a904ac1ac
MIQUELZAVALA <99816730+MIQUELZAVALA@users.noreply.github.com>
2022-05-27 14:32:20 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,12 @@
 #include "lab.h"
+#include "menu1.h"
 
 int main()
 {
     int tecla;
     //Bienvenida
-    int modo, contrarreloj, numerodejugadores, i;
-    char a, tiempo;
-    printf("\n\n\t\t\t\t------------BIENVENIDO AL JUEGO DEL LABERINTO------------\n\n");
-    printf("\n\n----En este juego vamos a comprobar de lo que eres capaz, y de si estarias preparado para salir de cualquier apuro.\n\n");
+    historia(1);
+    char a;
     printf("\n----Pulse cualquier enter para continuar. . . .");
     scanf("%c", &a);
     menu(1);
