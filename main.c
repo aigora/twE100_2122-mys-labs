144ab21852c9b7de4bd129424ae848b4354a78c5
MIQUELZAVALA <99816730+MIQUELZAVALA@users.noreply.github.com>
2022-05-27 21:57:36 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "lab.h"
+#include "menu1.h"
+#include "ficheros.h"
+
+int main()
+{
+    int tecla;
+    //Bienvenida
+    historia(1);
+    char a;
+    printf("\n----Pulse cualquier enter para continuar. . . .");
+    scanf("%c", &a);
+    menu(1);
+
+
+   return 0;
+}
