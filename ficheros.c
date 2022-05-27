5b99351a79110fe33effbd37d8504860c8327608
yeraymunozupm <99816468+yeraymunozupm@users.noreply.github.com>
2022-05-27 21:53:17 +0200

    Create ficheros.c

---
+++ ficheros.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void historia(int a)
+{
+    int i=0;
+    char letra[10000];
+    printf("\n\n");
+    FILE *pf;
+    pf = fopen("historia.txt", "r");
+
+    if(pf == NULL)
+        {
+            printf("Error al encontrar el archivo");
+            return (-1);
+        }
+
+    while(fscanf(pf, "%c", &letra[i]) != EOF)
+        {
+            printf("%c", letra[i]);
+            i++;
+        }
+    printf("\n\n");
+}
+
+void finalhistoria(int a)
+{
+    int i=0;
+    char letra[10000];
+    printf("\n\n");
+    FILE *pf;
+    pf = fopen("desenlacehistoria.txt", "r");
+
+    if(pf == NULL)
+        {
+            printf("Error al encontrar el archivo");
+            return (-1);
+        }
+
+    while(fscanf(pf, "%c", &letra[i]) != EOF)
+        {
+            printf("%c", letra[i]);
+            i++;
+        }
+    printf("\n\n");
+}
