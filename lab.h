bfd49bcc8d4f388258437ad900973c7a9e7c2b47
MIQUELZAVALA <99816730+MIQUELZAVALA@users.noreply.github.com>
2022-05-27 22:18:29 +0200

    Update lab.h

--- lab.h
+++ lab.h
@@ -4,10 +4,11 @@
 # include <stdio.h>
 # include "lab.c"
 
-void menu(int a);
-void ModoFacil(int a, int b);
-void ModoMedio(int a, int b);
-void ModoDificil(int a, int b);
-void ModoExtremo(int a, int b);
-void RecorrerLab(char a[20][20]);
+void ModoFacil(int a);
+void ModoMedio(int a);
+void ModoDificil(int a);
+void ModoCompeticion(int a);
+void ModoExtremo(int a);
+void RecorrerLab(int n, int m, char a[n][m]);
+void RecorrerLab2(int n, int m, char a[n][m]);
 #endif
