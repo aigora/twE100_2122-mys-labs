cb4e09453d7aff8b19d683e76470afb2a71a8636
yeraymunozupm <99816468+yeraymunozupm@users.noreply.github.com>
2022-06-26 20:59:57 +0200

    Update ficheros.c
    He añadido los fclose en casa función de fichero, para que no haya problemas
    He añadido una función valoración, que crea un fichero llamado valoración.txt, para ayudarnos a mejorar en función de la experiencia que tengan los jugadores.

--- ficheros.c
+++ ficheros.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "ficheros.h"
 
 void historia(int a)
 {
@@ -13,13 +14,16 @@ void historia(int a)
             printf("Error al encontrar el archivo");
             return (-1);
         }
-
+    else
+    {
     while(fscanf(pf, "%c", &letra[i]) != EOF)
         {
             printf("%c", letra[i]);
             i++;
         }
     printf("\n\n");
+    fclose(pf);
+    }
 }
 
 void finalhistoria(int a)
@@ -36,10 +40,40 @@ void finalhistoria(int a)
             return (-1);
         }
 
+    else
+    {
     while(fscanf(pf, "%c", &letra[i]) != EOF)
         {
             printf("%c", letra[i]);
             i++;
         }
     printf("\n\n");
+    fclose(pf);
+    }
+}
+
+void valoracionw(int a) //funcion escritura
+{
+    int i=0;
+    char texto[10000];
+    printf("Escribe que te ha parecido la experiencia en el laberinto:\n");
+    scanf(" %10000[^\n]", texto); //esto te lee un texto
+
+    FILE *pf;
+    pf = fopen("valoracion.txt", "w");
+
+    if(pf == NULL)
+        {
+            printf("Error al encontrar el archivo");
+            return (-1);
+        }
+    else
+    {
+    while(texto[i] != '\0')
+        {fprintf(pf, "%c", texto[i]);
+        i++;}
+
+        fclose(pf);
+    }
+    //printf("\n\n");
 }
