3d2db07ac974f3267df962060927bc5f1aef3e7a
MIQUELZAVALA <99816730+MIQUELZAVALA@users.noreply.github.com>
2022-06-30 00:22:40 +0200

    Update ficheros.c

--- ficheros.c
+++ ficheros.c
@@ -1,2 +1,47 @@
 #include <stdio.h>
+#include "ficheros.h"
+# include "lab.h"
 
+void historia(int a)
+{
+    int i=0;
+    char letra[10000];
+    printf("\n\n");
+    FILE *pf;
+    pf = fopen("historia.txt", "r");
+
+    if(pf == NULL)
+        {
+            printf("Error al encontrar el archivo");
+            return (-1);
+        }
+
+    while(fscanf(pf, "%c", &letra[i]) != EOF)
+        {
+            printf("%c", letra[i]);
+            i++;
+        }
+    printf("\n\n");
+}
+
+void finalhistoria(int a)
+{
+    int i=0;
+    char letra[10000];
+    printf("\n\n");
+    FILE *pf;
+    pf = fopen("desenlacehistoria.txt", "r");
+
+    if(pf == NULL)
+        {
+            printf("Error al encontrar el archivo");
+            return (-1);
+        }
+
+    while(fscanf(pf, "%c", &letra[i]) != EOF)
+        {
+            printf("%c", letra[i]);
+            i++;
+        }
+    printf("\n\n");
+}
