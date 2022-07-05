79e4d6e768358e73924bca600ecf361be50bbb68
yeraymunozupm <99816468+yeraymunozupm@users.noreply.github.com>
2022-07-05 12:58:13 +0200

    Update lab.h

--- lab.h
+++ lab.h
@@ -2,13 +2,13 @@
 #define _LAB_H_
 
 # include <stdio.h>
-# include "lab.c"
+//# include "lab.c"
 
 void ModoFacil(int a);
 void ModoMedio(int a);
 void ModoDificil(int a);
-void ModoCompeticion(int a);
+int ModoCompeticion(int a);
 void ModoExtremo(int a);
 void RecorrerLab(int n, int m, char a[n][m]);
-void RecorrerLab2(int n, int m, char a[n][m]);
+int RecorrerLab2(int n, int m, char a[n][m]);
 #endif
