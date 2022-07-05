6942ecd01515c71ad31ace4b295b31efc0f038c8
yeraymunozupm <99816468+yeraymunozupm@users.noreply.github.com>
2022-07-05 12:36:17 +0200

    Update ficheros.h

--- ficheros.h
+++ ficheros.h
@@ -2,11 +2,21 @@
 #define _FICHEROS_H_
 
 # include <stdio.h>
-# include "ficheros.c"
+//# include "ficheros.c"
+
+typedef struct
+{
+    char nombre[8];
+    int movimientos;
+    char experiencia[10000];
+}jugadores;
 
 void historia(int a);
 void finalhistoria(int a);
 void valoracionw(int a);
+void partidas(int a);
+void guardarpartida(int a);
 
 
 #endif
+
